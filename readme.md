d3ca2023aa88b4ba18d859da163d796698eb65ea
Deepanshu Kushwaha <56316738+Deepanshu2631@users.noreply.github.com>
2022-12-14 20:16:25 +0530

    first

---
+++ readme.md
@@ -0,0 +1,3 @@
+> # live-class-project-3
+
+![image](./live-class-project-3-ss.png)
---
+++ test.js
@@ -0,0 +1,21 @@
+// let arr = [1,2,3,4,6,7]
+// //spread
+// // let [a,b]=arr
+// console.log(a);
+// // spread and rest
+// let [a,b, ...deep]=arr
+// console.log(deep)
+
+
+// let arr = [1, 2, 3, "deepanshu", "hitesh"]
+// for(let i of arr){
+//     console.log(i)
+// }
+
+// let Obj={
+//     1:"deepanshu",
+//     2:"kushwaha"
+// }
+// for(let i in Obj){
+//     console.log(Obj[i])
+// }
